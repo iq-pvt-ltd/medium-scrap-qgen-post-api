5d9d04827461fe4bfeecca2cc8fffbfc4cc92d7b
THEJASWIN <r.k.thejaswin@gmail.com>
2022-01-24 18:07:30 +0530

    Added k8s domain

--- scrapper.py
+++ scrapper.py
@@ -1,10 +1,20 @@
-import re
 import json
 from selenium import webdriver
+from fastapi import FastAPI, Form
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-def scrap(inputLink):
+app = FastAPI()
+
+
+@app.get("/")
+def hello():
+    return ("API is working")
+
+
+@app.post("/medium/")
+def medium_scraper(inputLink: str = Form(...)):
     web = DesiredCapabilities.CHROME
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
@@ -12,8 +22,9 @@ def scrap(inputLink):
     options.add_argument("--disable-dev-shm-usage")
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
+    # Standalone chrome driver
     driver = webdriver.Remote(
-        command_executor='https://selenium-chrome-2o4gzaw5dq-el.a.run.app/wd/hub',
+        command_executor='https://selenium.vaathi.ai/wd/hub',
         desired_capabilities=web,
         options=options)
 
@@ -21,31 +32,29 @@ def scrap(inputLink):
     driver.delete_all_cookies()
     allContent = []
     driver.get(inputLink)
-
     try:
+        '''
+            The driver find the div my specified Xpath
+            and fetches title and content
+        '''
         article = driver.find_elements_by_xpath('//*[@id="root"]/div/div[3]/article/div/div/section[1]/div/div')
-        if len(article)>1:
-            article_title = article[0].find_elements_by_tag_name("h1")
-        else:
-            article = driver.find_elements_by_tag_name('section')
-            article_title = driver.find_element_by_tag_name('h1')
+        article_title = article[0].find_elements_by_tag_name("h1")
 
         for data in range(len(article)):
             temp_data = article[data].find_elements_by_tag_name("p")
             for text in temp_data:
                 allContent.append(text.text)
-        allPara = "".join(allContent)
 
+        allPara = "".join(allContent)
         '''
             The content is been formatted to JSON
         '''
         medium = {"Title":article_title[0].text,"Content":allPara}
-        output = json.dumps(medium,indent=2)
+        output = json.dumps(medium,indent=4)
         output_json = json.loads(output)
         '''
             Returns the JSON data
         '''
         return output_json
     finally:
-        print("......Exiting Medium.....")
         driver.quit()
