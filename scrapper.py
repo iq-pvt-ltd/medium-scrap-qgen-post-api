a9e70cd2852229dd994cb84bbcdea2b4d0f5ab93
THEJASWIN <r.k.thejaswin@gmail.com>
2022-01-24 18:11:37 +0530

    changes

--- scrapper.py
+++ scrapper.py
@@ -1,20 +1,10 @@
+import re
 import json
 from selenium import webdriver
-from fastapi import FastAPI, Form
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-app = FastAPI()
-
-
-@app.get("/")
-def hello():
-    return ("API is working")
-
-
-@app.post("/medium/")
-def medium_scraper(inputLink: str = Form(...)):
+def scrap(inputLink):
     web = DesiredCapabilities.CHROME
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
@@ -22,7 +12,6 @@ def medium_scraper(inputLink: str = Form(...)):
     options.add_argument("--disable-dev-shm-usage")
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-    # Standalone chrome driver
     driver = webdriver.Remote(
         command_executor='https://selenium.vaathi.ai/wd/hub',
         desired_capabilities=web,
@@ -32,29 +21,37 @@ def medium_scraper(inputLink: str = Form(...)):
     driver.delete_all_cookies()
     allContent = []
     driver.get(inputLink)
+
     try:
-        '''
-            The driver find the div my specified Xpath
-            and fetches title and content
-        '''
         article = driver.find_elements_by_xpath('//*[@id="root"]/div/div[3]/article/div/div/section[1]/div/div')
-        article_title = article[0].find_elements_by_tag_name("h1")
+        if len(article)==1:
+            article_title = article[0].find_elements_by_tag_name("h1")
+        else:
+            article = driver.find_elements_by_tag_name('section')
+            article_title = driver.find_element_by_tag_name('h1')
 
         for data in range(len(article)):
             temp_data = article[data].find_elements_by_tag_name("p")
             for text in temp_data:
                 allContent.append(text.text)
-
         allPara = "".join(allContent)
+
         '''
             The content is been formatted to JSON
         '''
         medium = {"Title":article_title[0].text,"Content":allPara}
-        output = json.dumps(medium,indent=4)
+        output = json.dumps(medium,indent=2)
         output_json = json.loads(output)
         '''
             Returns the JSON data
         '''
         return output_json
     finally:
+        print("......Exiting Medium.....")
         driver.quit()
+
+
+''''
+1)implememt Exception handling
+2)If x_path failed try section
+'''
