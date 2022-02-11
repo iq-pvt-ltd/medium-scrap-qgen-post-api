6fd701c2d6e753d46a4fb1c043841a153f95eb3b
THEJASWIN <48580367+THEJASWIN@users.noreply.github.com>
2022-02-11 12:53:59 +0530

    Revert "Updated Summary.py"
    This reverts commit 3f4c3fa3e38446cb77be75d71c84547691b6fb18.

--- summary.py
+++ summary.py
@@ -1,28 +1,23 @@
 import json
-from transformers import AutoConfig, AutoTokenizer, AutoModel
+import summarizer
+from transformers import AutoTokenizer, AutoModelForMaskedLM
 from summarizer import Summarizer
 from transformers import logging
 logging.set_verbosity_error()
 
 
-def summary(mediumText):
+def summary(contentMedium):
     '''
     SUMMARY FUNCTION
     '''
     PATH = './Sum_Model/bert-large-uncased/'
-    custom_config = AutoConfig.from_pretrained(PATH)
-    custom_config.output_hidden_states = True
-    custom_tokenizer = AutoTokenizer.from_pretrained(PATH)
-    custom_model = AutoModel.from_pretrained(PATH, config=custom_config)
-
-    Sum_model = Summarizer(custom_model=custom_model,
-                           custom_tokenizer=custom_tokenizer)
-                           
-    summary_content = Sum_model(mediumText, ratio=0.5)
+    summary_model = AutoModelForMaskedLM.from_pretrained(PATH)
+    summary_model = Summarizer()
+    summary_content = summary_model(contentMedium, ratio=0.5)
     fullSummarizedContent = ''.join(summary_content)
     summarizedContent = {"summaryContent": fullSummarizedContent}
     Json_summarizedContent = json.dumps(summarizedContent, indent=2)
     ''''
-        Returns the JSON data
+     Returns the JSON data
     '''
     return json.loads(Json_summarizedContent)
